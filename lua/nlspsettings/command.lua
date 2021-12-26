887fa102bb1c7f7194bc17de111444f28f8e034c
tamago324 <tamago_pad@yahoo.co.jp>
2021-12-26 19:01:19 +0900

    feat: add commands :NlspLocalConfig and :NlspLocalBufConfig

--- lua/nlspsettings/command.lua
+++ lua/nlspsettings/command.lua
@@ -1,10 +1,19 @@
 local config = require'nlspsettings.config'
 local nlspsettings = require'nlspsettings'
+local lspconfig_util = require'lspconfig.util'
 
 local a = vim.api
 local uv = vim.loop
 
 local M = {}
+
+--- Get the path to the buffer number
+---@param bufnr string
+---@return string
+local get_buffer_path = function(bufnr)
+  local expr = (bufnr ~= nil and '#' .. bufnr) or '%'
+  return vim.fn.expand(expr..':p')
+end
 
 --- Returns the name of the server connected to the current buffer.
 ---@param bufnr number? buffer number
@@ -44,33 +53,23 @@ local get_server_name = function(bufnr)
   return server_name
 end
 
---- Open a settings file that matches the current buffer
-M.open_buf_config = function()
-  local server_name = get_server_name()
-  if server_name == nil then
-    return
-  end
-
-  M.open_config(server_name)
-end
-
----Open the settings file for the specified server.
+--- open config file
 ---@param server_name string
-M.open_config = function(server_name)
+---@param dir string
+local open_config = function(dir, server_name)
   vim.validate {
-    server_name = { server_name, 's' }
+    server_name = { server_name, 's' },
+    dir = { dir, 's' }
   }
 
-  local home = config.get('config_home')
-
-  if vim.fn.isdirectory(home) == 0 then
-    if vim.fn.confirm(string.format([[Config directory "%s" not exists, create?]], home), "&Yes\n&No", 1) ~= 1 then
+  if vim.fn.isdirectory(dir) == 0 then
+    if vim.fn.confirm(string.format([[Config directory "%s" not exists, create?]], dir), "&Yes\n&No", 1) ~= 1 then
       return
     end
-    vim.fn.mkdir(home, 'p')
+    vim.fn.mkdir(dir, 'p')
   end
 
-  local path = string.format('%s/%s.json', home, server_name)
+  local path = string.format('%s/%s.json', dir, server_name)
 
   -- If the file does not exist, LSP will not be able to complete it, so create it
   do
@@ -86,6 +85,56 @@ M.open_config = function(server_name)
 
   local cmd = (vim.api.nvim_buf_get_option(0, 'modified') and 'split') or 'edit'
   vim.api.nvim_command(string.format([[%s %s]], cmd, path))
+end
+
+--- Open a settings file that matches the current buffer
+M.open_buf_config = function()
+  local server_name = get_server_name()
+  if server_name == nil then
+    return
+  end
+
+  M.open_config(server_name)
+end
+
+---Open the settings file for the specified server.
+---@param server_name string
+M.open_config = function(server_name)
+  open_config(config.get('config_home'), server_name)
+end
+
+---Open the settings file for the specified server.
+---@param server_name string
+M.open_local_config = function(server_name)
+  local start_path = get_buffer_path()
+  if start_path == '' then
+    start_path = vim.fn.getcwd()
+  end
+
+  local markers = config.get('local_settings_root_markers') or {}
+  local root_dir = lspconfig_util.root_pattern(markers)(start_path)
+  if root_dir == nil then
+    a.nvim_echo({{string.format(' Nlsp[%s][Error] Failed to get root_dir.', server_name), 'ErrorMsg'}}, false, {})
+    return
+  end
+  open_config(string.gsub(root_dir, '/$', '') .. '/.nlsp-settings', server_name)
+end
+
+--- Open a settings file that matches the current buffer
+M.open_local_buf_config = function()
+  local server = get_server_name()
+  if server == nil then
+    return
+  end
+
+  local client = vim.lsp.get_client_by_id(server.client_id)
+  if client == nil then
+    a.nvim_echo({{string.format(' Nlsp[%s][Error] Failed to get root_dir.', server.name), 'ErrorMsg'}}, false, {})
+    return
+  end
+  local root_dir = client.config.root_dir
+
+  open_config(root_dir .. '/.nlsp-settings', server.name)
 end
 
 ---Update the setting values.
--- plugin/nlspsetting.vim
+++ plugin/nlspsetting.vim
@@ -8,8 +8,13 @@ function! s:complete(arg, line, pos) abort
 endfunction
 
 command! -nargs=1 -complete=custom,s:complete NlspConfig lua require('nlspsettings.command').open_config(<f-args>)
-command! -nargs=1 -complete=custom,s:complete NlspUpdateSettings lua require('nlspsettings.command').update_settings(<f-args>)
+command! -nargs=1 -complete=custom,s:complete NlspLocalConfig lua require('nlspsettings.command').open_local_config(<f-args>)
+
 command! -nargs=0 NlspBufConfig lua require('nlspsettings.command').open_buf_config()
+command! -nargs=0 NlspLocalBufConfig lua require('nlspsettings.command').open_local_buf_config()
+
+command! -nargs=1 -complete=custom,s:complete NlspUpdateSettings lua require('nlspsettings.command').update_settings(<f-args>)
 
 nnoremap <Plug>(nlsp-buf-config) <Cmd>lua require('nlspsettings.command').open_buf_config()<CR>
+nnoremap <Plug>(nlsp-local-buf-config) <Cmd>lua require('nlspsettings.command').open_local_buf_config()<CR>
 
